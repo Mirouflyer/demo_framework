c055aa94a821fe1a30af2c97e8a2bb8d10c8a5a8
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-17 17:57:53 -0400

    basic framework

---
+++ Generic/src/main/java/CommonAPI.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonAPI {
+
+    public WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.drive","/Users/mirouflyer/mirou-workspace/DemoTestFramework/Generic/driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to("https://www.google.com/");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void cleanUO(){
+        driver.close();
+    }
+}
